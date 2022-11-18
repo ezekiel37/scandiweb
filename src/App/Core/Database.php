e6c8b41b63e8fe2ab4b0cca30178aec9878dafa0
ezekiel37 <ezekielojo37@gmail.com>
2022-11-18 08:57:42 +0100

    wel

--- src/App/Core/Database.php
+++ src/App/Core/Database.php
@@ -3,6 +3,7 @@
 namespace App\Core;
 
 use PDO;
+use PDOException;
 
 class Database
 {
@@ -11,21 +12,54 @@ class Database
         private string $name,
         private string $user,
         private string $password
-    )
-    {
+    ) {
     }
 
     public function getConnection(): PDO
     {
-        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
-        return new PDO($dsn, $this->user, $this->password, [
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+            return new PDO($dsn, $this->user, $this->password, [
 
-            PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::ATTR_STRINGIFY_FETCHES => false
-        ]);
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false
+            ]);
+        } catch (PDOException $e) {
+           echo die($e->getMessage());
+        }
 
-        echo $this->host;
-        echo $this->password;
-       
+        //         echo $this->host;
+        // echo $this->password;
     }
 }
+
+// namespace App\core;
+// use PDO;
+// use PDOException;
+
+// class Database
+// {
+//     public static $con;
+//     private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+
+//     public function __construct()
+//     {
+//         try{
+//             $string = "mysql:host=Localhost; dbname=scandiweb";
+//             self::$con = new PDO($string, "scandiweb", "scandiweb001", $this->options);
+//         }
+//         catch (PDOException $e){
+//             die($e->getMessage());
+//         }
+//     }
+
+//     public static function getInstance()
+//     {
+//         if(self::$con){
+//             return self::$con;
+//         }
+
+//         self::$con = new self();
+//         return self::$con;
+//     }
+// }
